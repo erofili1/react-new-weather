bac2a3b6bdb4834356b58f19554bd33dd11a7d48
erofili1 <erixaberi@gmail.com>
2022-07-06 11:08:14 +0100

    created react-app directly into folder (NOT my-app)

---
+++ src/Weather.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
+
+
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState("");
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setLoaded(false);
+    const apiKey = `7654bb3646824703bcfdf4ced8409f03`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  const form = (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="search"
+        placeholder="Enter a city..."
+        onChange={updateCity}
+      />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>
+            <strong>{weather.city}</strong>
+          </li>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {Math.round(weather.wind)}kph</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+        <p><a href="https://github.com/erofili1/react-weather-app">Open source code</a> by Erofili Pateraki</p>
+      </div>
+    );
+  }
+  
+  return (
+    <div>
+      {form}
+      <p><a href="https://github.com/erofili1/react-weather-app">Open source code</a> by Erofili Pateraki</p>
+
+    </div>
+    );
+}
